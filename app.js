7f138736412fadc6e5995bb035642ac5d45e81a3
Eduard <eduardrg@uw.edu>
2015-07-08 04:56:44 -0700

    Redirect URL bug attempt 2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var tokenGetter = function() {
   } else {
     if (confirm('You must sign in to Spotify to continue.')) {
       var CLIENT_ID = 'fa96e83e7cfd46759a5179a204181039';
-      var successURL = currentURL;
+      var successURL = 'https://students.washington.edu/eduardrg/info343/spotify-challenge/index.html';
       var scopes = encodeURIComponent('playlist-read-private playlist-read-collaborative');
       window.location.assign('https://accounts.spotify.com/authorize' +
           '?response_type=token' + '&client_id=' + CLIENT_ID + '&scope='
